54035773113f26581a0ad097dab0633dfb00a3f9
eremiticengineer <dev@eremiticengineer.com>
2026-08-27 11:51:07 +0100

    Implement average wind speed and gusts

--- src/sn74hc14n-test.cpp
+++ src/sn74hc14n-test.cpp
@@ -5,46 +5,66 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#include "WindMonitor.hpp"
+
 #define MAIN_TASK_PRIORITY ( tskIDLE_PRIORITY + 2UL )
 
-const uint SN74HC14N_INTERRUPT_PIN = 14;
+const uint SN74HC14N_INTERRUPT_PIN = 15;
 const bool CALLBACK_ENABLED = true;
 int sn74hc14n_count = 0; // don't need volatile as only the task access it, not the ISR
 
 TaskHandle_t sn74hc14n_task_handle = nullptr;
 
+static WindMonitor wind;
+
 void sn74hc14n_callback(uint gpio, __unused uint32_t events) {
   if (gpio == SN74HC14N_INTERRUPT_PIN) {
-      BaseType_t higher_priority_task_woken = pdFALSE;
-      vTaskNotifyGiveFromISR(sn74hc14n_task_handle, &higher_priority_task_woken);
-      portYIELD_FROM_ISR(higher_priority_task_woken);
+      wind.onPulse();
   }
 }
 
-// vTaskNotifyGiveFromISR() increments the task's notification value each time the interrupt occurs.
-// So if three interrupts happen before the task gets CPU time,
-// FreeRTOS can remember that there were three notifications. But:
-//   ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-// returns the number of notifications that accumulated and then clears the count.
-// So take advantage of that.
-// Suppose SN74HC14N_INTERRUPT_PIN gets:
-//   interrupt
-//   interrupt
-//   interrupt
-// before the task runs, the notification value becomes 3. Then:
-//   uint32_t pulses = ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
-// gives:
-//   pulses == 3
-// so:
-//   sn74hc14nCount += pulses;
-// doesn't lose those pulses.
-void sn74hc14n_task(void *pvParameters) {
-    while (true) {
-        uint32_t pulses = ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+void wind_scheduler_task(__unused void *)
+{
+    const uint32_t INTERVAL_MS = 1000;
+    const TickType_t tickDelay = pdMS_TO_TICKS(INTERVAL_MS);
 
-        sn74hc14n_count += pulses;
+    uint32_t seconds = 0;
 
-        printf("SN74HC14N : %d\n", sn74hc14n_count);
+    while (true)
+    {
+        wind.sampleAverage1s();
+
+        // Calculate gust over each 2-second period.
+        if ((seconds + 1) % 2 == 0)
+        {
+            wind.sampleGustInterval(2000);
+        }
+
+        // Move to the next minute slot every 60 seconds.
+        if ((seconds + 1) % 60 == 0)
+        {
+            wind.rotateMinute();
+        }
+
+        seconds++;
+
+        vTaskDelay(tickDelay);
+    }
+}
+
+void wind_info_task(__unused void *)
+{
+    while (true)
+    {
+        const int32_t average = wind.getRunningAverageMph10();
+
+        const int32_t gust = wind.getHourlyMaxGustMph10();
+
+        printf("Average wind: %ld.%ld mph\n", static_cast<long>(average / 10), static_cast<long>(average % 10));
+
+        printf("Max gust: %ld.%ld mph\n", static_cast<long>(gust / 10), static_cast<long>(gust % 10));
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
 
@@ -58,7 +78,8 @@ int main(void)
     gpio_set_irq_enabled_with_callback(SN74HC14N_INTERRUPT_PIN, GPIO_IRQ_EDGE_RISE,
         CALLBACK_ENABLED, sn74hc14n_callback);
 
-    xTaskCreate(sn74hc14n_task, "SN74HC14N", 1024, nullptr, MAIN_TASK_PRIORITY, &sn74hc14n_task_handle);
+    xTaskCreate(wind_scheduler_task, "WindSchedulerTask", 1024, nullptr, MAIN_TASK_PRIORITY, nullptr);
+    xTaskCreate(wind_info_task, "WindInfoTask", 1024, nullptr, MAIN_TASK_PRIORITY, nullptr);
 
     vTaskStartScheduler();
 
